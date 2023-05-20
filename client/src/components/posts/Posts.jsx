a73424a6bb777f72fd905c33ac54c63970d12b6a
Azhar-Ali11 <azharali2018123@gmail.com>
2023-05-20 07:57:13 -0700

    key issue fixed

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -2,10 +2,15 @@ import Post from "../post/Post";
 import "./posts.css";
 
 export default function Posts({ posts }) {
+  if (!Array.isArray(posts)) {
+    // Handle the case when `posts` is not an array
+    return <div>No posts to display</div>;
+  }
+
   return (
     <div className="posts">
       {posts.map((p) => (
-        <Post post={p} />
+        <Post post={p} key={p.id} /> // Don't forget to add a unique key prop
       ))}
     </div>
   );
