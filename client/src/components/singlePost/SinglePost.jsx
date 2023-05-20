0b4c38029c1755993608696c21ed91b8030e1673
Azhar-Ali11 <azharali2018123@gmail.com>
2023-05-20 07:38:34 -0700

    update1

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -17,7 +17,7 @@ export default function SinglePost() {
 
   useEffect(() => {
     const getPost = async () => {
-      const res = await axios.get("https://tiny-jade-rhinoceros-boot.cyclic.app/api/posts/" + path);
+      const res = await axios.get("/posts/" + path);
       setPost(res.data);
       setTitle(res.data.title);
       setDesc(res.data.desc);
